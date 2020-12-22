bd3ae001df16b4a692c3ea223735c4256c48a2b4
hous <304536797@qq.com>
2020-12-22 23:46:01 +0800

    update

--- fast_lib/src/main/java/com/hstmpl/aspectj/AsyncAspect.java
+++ fast_lib/src/main/java/com/hstmpl/aspectj/AsyncAspect.java
@@ -1,42 +1,42 @@
-package com.hstmpl.aspectj;
-
-
-import com.hstmpl.aspectj.annotation.Async;
-import com.hstmpl.util.ThreadPools;
-
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
-import org.aspectj.lang.reflect.MethodSignature;
-
-import java.lang.reflect.Method;
-
-@Aspect
-public class AsyncAspect {
-
-
-    @Pointcut("execution(@com.ws.fastlib.aspectj.annotation.Async * *(..))")
-    public void asyncMethod() {
-    }
-
-    @Around("asyncMethod()")
-    public void aroundJoinPoint(final ProceedingJoinPoint joinPoint) throws Throwable {
-        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
-        Method method = methodSignature.getMethod();
-        if (!method.isAnnotationPresent(Async.class)) {
-            return;
-        }
-
-        ThreadPools.execute(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    joinPoint.proceed();
-                } catch (Throwable throwable) {
-                    throw new RuntimeException(throwable.getMessage());
-                }
-            }
-        });
-    }
-}
+//package com.hstmpl.aspectj;
+//
+//
+//import com.hstmpl.aspectj.annotation.Async;
+//import com.hstmpl.util.ThreadPools;
+//
+//import org.aspectj.lang.ProceedingJoinPoint;
+//import org.aspectj.lang.annotation.Around;
+//import org.aspectj.lang.annotation.Aspect;
+//import org.aspectj.lang.annotation.Pointcut;
+//import org.aspectj.lang.reflect.MethodSignature;
+//
+//import java.lang.reflect.Method;
+//
+//@Aspect
+//public class AsyncAspect {
+//
+//
+//    @Pointcut("execution(@com.ws.fastlib.aspectj.annotation.Async * *(..))")
+//    public void asyncMethod() {
+//    }
+//
+//    @Around("asyncMethod()")
+//    public void aroundJoinPoint(final ProceedingJoinPoint joinPoint) throws Throwable {
+//        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+//        Method method = methodSignature.getMethod();
+//        if (!method.isAnnotationPresent(Async.class)) {
+//            return;
+//        }
+//
+//        ThreadPools.execute(new Runnable() {
+//            @Override
+//            public void run() {
+//                try {
+//                    joinPoint.proceed();
+//                } catch (Throwable throwable) {
+//                    throw new RuntimeException(throwable.getMessage());
+//                }
+//            }
+//        });
+//    }
+//}
